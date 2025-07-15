09d901d12ffa496fd9fd357c635c673dd46deeac
amit negi <amitnegi@gmail.com>
2025-07-15 19:12:14 +0530

    add revision files3

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-# login to AWS ECR
 
-aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 888121514404.dkr.ecr.eu-north-1.amazonaws.com
-docker pull 888121514404.dkr.ecr.eu-north-1.amazonaws.com/onway_ecr:v3
-docker stop my-container || true
-docker rm my-container || true
-docker run -d -p 80:5000 -e DAGSHUB_PAT=c900d827474e85a0334daa0d446a8218d2b9f611 --name onway-app 888121514404.dkr.ecr.eu-north-1.amazonaws.com/onway_ecr:v3
+# Login to AWS ECR
+sudo aws ecr get-login-password --region eu-north-1 | \
+sudo docker login --username AWS --password-stdin 888121514404.dkr.ecr.eu-north-1.amazonaws.com
 
+# Pull the Docker image
+sudo docker pull 888121514404.dkr.ecr.eu-north-1.amazonaws.com/onway_ecr:v3
+
+# Stop and remove old container
+sudo docker stop onway-app || true
+sudo docker rm onway-app || true
+
+# Run the Docker container in detached mode
+sudo docker run -d -p 80:5000 \
+  -e DAGSHUB_PAT=c900d827474e85a0334daa0d446a8218d2b9f611 \
+  --name onway-app \
+  888121514404.dkr.ecr.eu-north-1.amazonaws.com/onway_ecr:v3
