776c211c656fd3578d14d5e49586c5558ffa977b
amit negi <amitnegi@gmail.com>
2025-07-15 18:55:09 +0530

    add revision files2

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -5,5 +5,5 @@ aws ecr get-login-password --region eu-north-1 | docker login --username AWS --p
 docker pull 888121514404.dkr.ecr.eu-north-1.amazonaws.com/onway_ecr:v3
 docker stop my-container || true
 docker rm my-container || true
-docker run -p 80:5000 -e DAGSHUB_PAT=c900d827474e85a0334daa0d446a8218d2b9f611 --name onway-app 888121514404.dkr.ecr.eu-north-1.amazonaws.com/onway_ecr:v3
+docker run -d -p 80:5000 -e DAGSHUB_PAT=c900d827474e85a0334daa0d446a8218d2b9f611 --name onway-app 888121514404.dkr.ecr.eu-north-1.amazonaws.com/onway_ecr:v3
 
