179f340ab3b4960fdf4b6f57ab2530c90ef31740
amit negi <amitnegi@gmail.com>
2025-07-17 01:02:18 +0530

    add new changes

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -15,4 +15,4 @@ sudo docker rm onway-app || true
 sudo docker run -d -p 80:5000 \
   -e DAGSHUB_PAT=c900d827474e85a0334daa0d446a8218d2b9f611 \
   --name onway-app \
-  888121514404.dkr.ecr.eu-north-1.amazonaws.com/onway_ecr:v4
+  888121514404.dkr.ecr.eu-north-1.amazonaws.com/onway_ecr:v6
